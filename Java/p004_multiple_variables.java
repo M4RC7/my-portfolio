5643f21b6b486229d7c5da42112befe48ee03a17
M4RC7 <36773969+M4RC7@users.noreply.github.com>
2022-09-22 22:00:56 -0300

    Create p004_multiple_variables.java

---
+++ Java/p004_multiple_variables.java
@@ -0,0 +1,12 @@
+public class p004_multiple_variables{
+  public static void main(String[] args){
+    //many variables of same type in one line
+    int x = 5, y = 6, z = 50;
+    System.out.println(x + y + z);
+    
+    //one value for multiple variables
+    int w, v, u;
+    w = v = u = 50;
+    System.out.println(w + v + u);
+  }
+}
