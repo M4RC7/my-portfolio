be474d83fc8741817d4c00eb7ad88956e34f5e40
M4RC7 <36773969+M4RC7@users.noreply.github.com>
2022-09-22 22:01:12 -0300

    Update p002_variables.java

--- Java/p002_variables.java
+++ Java/p002_variables.java
@@ -19,5 +19,25 @@ public class p002_variables{
     final int myNumFinal = 15;
     //myNumFinal = 20; //yields error
     System.out.println(myNumFinal);
+    
+    /*
+    All variables must be identified with unique names called identifiers.
+    Identifiers can be short names (like x and y) or more descriptive names (age, sum, totalVolume).
+    It is recommended to use descriptive names in order to create understandable and maintainable code:
+    -----
+    // Good
+    int minutesPerHour = 60;
+
+    // OK, but not so easy to understand what m actually is
+    int m = 60;
+    ------
+    The general rules for naming variables are:
+    Names can contain letters, digits, underscores, and dollar signs;
+    Must begin with a letter;
+    Should start with a lowercase letter and it cannot contain whitespace;
+    Can also begin with $ and _ (but we will not use it in this tutorial);
+    Are case sensitive ("myVar" and "myvar" are different variables);
+    Reserved words, such as int or boolean, cannot be used as names.
+    */
   }
 }
