f1861255731fa27e22595636ff5df0f5666c8fa6
M4RC7 <36773969+M4RC7@users.noreply.github.com>
2022-09-22 21:34:36 -0300

    Create 002_variables.java

---
+++ Java/002_variables.java
@@ -0,0 +1,23 @@
+public class 002_variables{
+  public static void main(String[] args){
+    int myNum = 5;
+    float myFloatNum = 5.99f;
+    char myLetter = 'D';
+    boolean myBool = true;
+    String myText = "Hello";
+    System.out.println(myNum);
+    System.out.println(myFloatNum);
+    System.out.println(myLetter);
+    System.out.println(myBool);
+    System.out.println(myText);
+    
+    //changing a value
+    myNum = 20;
+    System.out.println(myNum);
+    
+    //final variables cannot have their values changed
+    final int myNumFinal = 15;
+    //myNumFinal = 20; //yields error
+    System.out.println(myNumFinal);
+  }
+}
