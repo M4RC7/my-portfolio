5c480b2e814ee5aefe9197e68d9127e829cd821d
M4RC7 <36773969+M4RC7@users.noreply.github.com>
2022-09-22 21:53:32 -0300

    Create p003_variable_combos.java

---
+++ Java/p003_variable_combos.java
@@ -0,0 +1,25 @@
+public class p003_variable_combos{
+  public static void main(String[] args){
+    int myNum = 5;
+    float myFloatNum = 5.99f;
+    char myLetter = 'D';
+    boolean myBool = true;
+    String myText = "Hello";
+    System.out.println("Hi "+myText);
+    
+    //text and variable
+    String myName = "Hi and "+myText;
+    System.out.println(myName);
+    
+    //adding variable to variable
+    String myName2 = myText+myLetter;
+    System.out.println(myName2);
+    
+    //math operator
+    final int myNumFinal = 15;
+    System.out.println(myNumFinal+myNum);
+    
+    //override
+    System.out.println(myFloatNum+myNum);
+  }
+}
