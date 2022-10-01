fbc3a7f0b4fd9424dc75dc6f4fad4851bf2fe8df
M4RC7 <36773969+M4RC7@users.noreply.github.com>
2022-09-30 22:15:52 -0300

    Update p006a_concatenation.java

--- Java/p006a_concatenation.java
+++ Java/p006a_concatenation.java
@@ -4,5 +4,31 @@ public class p006a_concatenation{
     String lastName = "Doe";
     System.out.println(firstName + " " + lastName);
     System.out.println(firstName.concat(lastName));
+    
+    //Addition vs. Concatenation
+    int x = 10;
+    int y = 20;
+    int z = x + y;
+    System.out.println(z);
+    
+    String a = "10";
+    String b = "20";
+    String c = a + b;
+    System.out.println(c);
+    
+    String d = a + y;
+    System.out.println(d);
+    
+    /*
+    Special Characters:
+    \' ' Single quote
+    \" " Double quote
+    \\ \ Backslash
+    \n	New Line	
+    \r	Carriage Return	
+    \t	Tab	
+    \b	Backspace	
+    \f	Form Feed
+    */
   }
 }
