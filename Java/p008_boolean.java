979d1399f704b52cd57fb182b24f8e06eb8b2102
M4RC7 <36773969+M4RC7@users.noreply.github.com>
2022-10-02 15:18:52 -0300

    Create p008_boolean.java

---
+++ Java/p008_boolean.java
@@ -0,0 +1,14 @@
+public class p008_boolean{
+  public static void main(String[] args){
+    //raw boolean
+    boolean fact = true;
+    boolean fake = false;    
+    System.out.println(fact);
+    System.out.println(fake);
+    
+    //conditional testing
+    System.out.println(10 > 9);
+    System.out.println(10 != 10);
+    System.out.println(10 == 15);
+  }
+}
