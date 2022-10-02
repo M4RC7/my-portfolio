1916f131b85c65fdabd3f99a22ca34eefbafce0a
M4RC7 <36773969+M4RC7@users.noreply.github.com>
2022-10-02 20:06:00 -0300

    Create p012_for.java

---
+++ Java/p012_for.java
@@ -0,0 +1,9 @@
+public class p012_for {
+  public static void main(String[] args) {
+    //assign a value, while condition is true, assign a value
+    for (int i = 0; i <= 10; i = i + 2) {
+      System.out.println(i);
+    }
+    //looping an array -check "arguments.java"
+  }
+}
