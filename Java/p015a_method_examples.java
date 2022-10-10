a2c6b88d15ac4b1ec4b15fe76dc2f057116e4ca8
M4RC7 <36773969+M4RC7@users.noreply.github.com>
2022-10-10 09:49:36 -0300

    Create p015a_method_examples.java

---
+++ Java/p015a_method_examples.java
@@ -0,0 +1,19 @@
+public class p015a_method_examples {
+  static void checkNum(int x) {
+    if (x < 18) {
+      System.out.println("Below 20");
+    } else {
+      System.out.println("20 or greater");
+    }
+  }
+  
+  static int add(int x, int y) {
+    return x + y;
+  }
+
+  public static void main(String[] args) {
+    int a = add(17, 3);
+    System.out.println(a);
+    checkNum(a);
+  }
+}
